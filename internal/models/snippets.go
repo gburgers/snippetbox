dbfb39024570564a3730d07e1b80ac1c92365918
gburgers <gerben.burgers@gmail.com>
2025-03-16 19:50:00 +0100

    edit snippets for pg

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -32,28 +32,38 @@ func (m *SnippetModel) Insert(title string, content string, expires int) (int, e
 
 	// To PostgreSQL syntax:
 	stmt := `INSERT INTO snippets (title, content, created, expires)
-    VALUES($1, $2, CURRENT_TIMESTAMP AT TIME ZONE 'UTC', (CURRENT_TIMESTAMP AT TIME ZONE 'UTC') + ($3 || ' days')::interval)`
+    VALUES($1, $2, CURRENT_TIMESTAMP AT TIME ZONE 'UTC', (CURRENT_TIMESTAMP AT TIME ZONE 'UTC') + ($3 || ' days')::interval) RETURNING id`
+
+	// Instead of Exec(), use QueryRow() to get the returned id
+	var id int
+	err := m.DB.QueryRow(stmt, title, content, expires).Scan(&id)
+	if err != nil {
+		return 0, err
+	}
+
+	// Return the id directly
+	return id, nil
 
 	// Use the Exec() method on the embedded connection pool to execute the
 	// statement. The first parameter is the SQL statement, followed by the
 	// values for the placeholder parameters: title, content and expiry in
 	// that order. This method returns a sql.Result type, which contains some
 	// basic information about what happened when the statement was executed.
-	result, err := m.DB.Exec(stmt, title, content, expires)
-	if err != nil {
-		return 0, err
-	}
+	// result, err := m.DB.Exec(stmt, title, content, expires)
+	// if err != nil {
+	// return 0, err
+	// }
 
 	// Use the LastInsertId() method on the result to get the ID of our
 	// newly inserted record in the snippets table.
-	id, err := result.LastInsertId()
-	if err != nil {
-		return 0, err
-	}
+	// id, err := result.LastInsertId()
+	// if err != nil {
+	// return 0, err
+	// }
 
 	// The ID returned has the type int64, so we convert it to an int type
 	// before returning.
-	return int(id), nil
+	// return int(id), nil
 }
 
 // This will return a specific snippet based on its id.
